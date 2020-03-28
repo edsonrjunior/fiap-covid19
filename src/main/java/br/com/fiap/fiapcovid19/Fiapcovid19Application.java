c311ed6e48ca2e3acbfb0834c0f1b61a7fbb699c
Hugo <hodestito@gmail.com>
2020-03-28 09:55:56 -0300

    First commit

---
+++ src/main/java/br/com/fiap/fiapcovid19/Fiapcovid19Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapcovid19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fiapcovid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fiapcovid19Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/fiapcovid19/Fiapcovid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapcovid19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fiapcovid19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
