15c3b0a097d3bea7b7815e11ed075b4066ecf293
Richel Sensineli <richelsensineli@Richels-MacBook-Pro.local>
2020-03-28 11:22:57 -0300

    Adicionando métodos no repository, e criação da classe service

--- src/main/java/br/com/fiap/fiapcovid19/repository/DoadorRepository.java
+++ src/main/java/br/com/fiap/fiapcovid19/repository/DoadorRepository.java
@@ -6,7 +6,12 @@ import br.com.fiap.fiapcovid19.model.Doador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface DoadorRepository extends JpaRepository<Doador, Integer> { 
-    
+public interface DoadorRepository extends JpaRepository<Doador, Integer> {
+
+    public List<Doador> findByTipoSanguineo(String tipoSanguineo);
+    public List<Doador> findByCidade(String cidade);
+
 }
